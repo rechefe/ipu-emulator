973012b12c4daf030a439ae6bdacd175bc0bc5cd
eyalrr2 <eyalrr2@gmail.com>
2026-01-19 18:43:42 +0200

    [emulator] Add ipu_acc module

---
+++ src/lib/ipu/ipu_acc_inst.c
@@ -0,0 +1,8 @@
+#include "ipu_acc_inst.h"
+
+void ipu__execute_acc_instruction(ipu__obj_t *ipu,
+                                  inst_parser__inst_t inst,
+                                  const ipu__regfile_t *regfile_snapshot)
+{
+
+}
